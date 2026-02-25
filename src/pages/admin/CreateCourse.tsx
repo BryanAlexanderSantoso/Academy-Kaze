3e6a30d65a4aec6d54f6b28186a3221d240a88da
Macbook Air <macbookair@Macbooks-MacBook-Air-2.local>
2026-02-25 11:46:04 +0700

    bug kontol

--- src/pages/admin/CreateCourse.tsx
+++ src/pages/admin/CreateCourse.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../services/api';
-import type { Course } from '../../lib/supabase';
+
 import { motion } from 'framer-motion';
 import {
   Save,
